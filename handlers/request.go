3db423d084dddd1bbf2a4f1265ed9fa21fbc5d47
jcardenasc93 <jcardenasc93@gmail.com>
2023-06-27 22:34:25 -0500

    Refactor request handler

--- handlers/request.go
+++ handlers/request.go
@@ -5,23 +5,26 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
-
-type httpVerb int
 
 const (
-	HTTPVerbGet httpVerb = iota
-	HTTPVerbPost
-	HTTPVerbPut
-	HTTPVerbDelete
+	HTTPGet    string = http.MethodGet
+	HTTPPost   string = http.MethodPost
+	HTTPPut    string = http.MethodPut
+	HTTPDelete string = http.MethodDelete
 )
-const defaultVerb = "GET"
+const defaultVerb = HTTPGet
 
-var httpVerbs map[string]httpVerb = map[string]httpVerb{
-	"GET":    HTTPVerbGet,
-	"POST":   HTTPVerbPost,
-	"PUT":    HTTPVerbPut,
-	"DELETE": HTTPVerbDelete,
+var strings map[string]string = map[string]string{
+	"GET":    HTTPGet,
+	"get":    HTTPGet,
+	"POST":   HTTPPost,
+	"post":   HTTPPost,
+	"PUT":    HTTPPut,
+	"put":    HTTPPut,
+	"DELETE": HTTPDelete,
+	"delete": HTTPDelete,
 }
 
 type RequestHandler struct {
@@ -37,21 +40,26 @@ func new(url string, contentType string) *RequestHandler {
 	}
 }
 
-func MakeRequest(url, contentType, verb string) (resp *http.Response, err error) {
+func newReq(method string, h *RequestHandler) (r *http.Request) {
+	r, err := http.NewRequest(method, h.url, h.ioReader)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return
+}
+
+func MakeRequest(url, contentType, method string) (resp *http.Response, err error) {
 	handler := new(url, contentType)
 	client := &http.Client{}
-	v, ok := httpVerbs[verb]
+	m, ok := strings[method]
 	if ok != true {
 		return resp, errors.New("Invalid HTTP verb")
 	}
-	switch v {
-	case HTTPVerbGet:
-		resp, err = client.Get(handler.url)
-	case HTTPVerbPost:
-		resp, err = client.Post(handler.url, handler.contentType, handler.ioReader)
-	default:
-		return resp, errors.New("Invalid HTTP verb")
-	}
+	req := newReq(m, handler)
+	req.Header.Set("Content-type", handler.contentType)
+	resp, err = client.Do(req)
+
 	if err != nil {
 		fmt.Printf("Something get wrong. %s\n", err.Error())
 		return resp, err
